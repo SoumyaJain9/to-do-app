c999acfed97dff734ed82e4d7254e3e9a1e05faf
Soumya Jain <soumya09062006@gmail.com>
2026-07-17 09:47:18 +0530

    connected frontend wiht backend

--- todo-frontend/src/App.jsx
+++ todo-frontend/src/App.jsx
@@ -1,8 +1,7 @@
 import { useEffect, useState } from 'react'
 import axios from 'axios'
 
-const API = 'http://localhost:3000'
-
+const API = 'https://api.soumyajain.online'
 const cardColors = [
   'bg-primary-fixed-dim/30',
   'bg-secondary-container/20',
